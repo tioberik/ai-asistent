4c54efee5ac51a91c7482fde08b1a8a190bf47b6
root <root@vmi1559600.contaboserver.net>
2024-03-19 10:40:24 +0100

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(app)
 
 
@@ -14,6 +16,11 @@ api.add_resource(AssistantApi, "/ask")
 from assistant_feedback import AssistantFeedback
 api.add_resource(AssistantFeedback, "/feedback")
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
